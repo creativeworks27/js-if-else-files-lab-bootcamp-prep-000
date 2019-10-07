a5b6eeaae782f5788b72d032f9c579d0946c9071
Roberto Romero <creativeworks27@icloud.com>
2019-10-07 01:48:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ if (name === "Sussan"){
   console.log("Expected the name to be Susan")
 }
 
+if (height === "70"){
+  console.log("The height is correct")
+}
+
 
 // Don't worry about this
 module.exports = { name, height
